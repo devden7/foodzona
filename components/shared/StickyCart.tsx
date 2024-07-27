bdd415d20a2ed41d86b72500054f569919ba1ad9
devden7 <devdenn7@gmail.com>
2024-07-27 13:41:54 +0700

    fix: adjustment cart page

--- components/shared/StickyCart.tsx
+++ components/shared/StickyCart.tsx
@@ -4,9 +4,9 @@ import { HiShoppingBag } from 'react-icons/hi';
 
 const StickyCart = () => {
   return (
-    <div className="sticky bottom-0 z-50 h-20 w-full bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+    <div className=" sticky bottom-0 z-50 h-20 w-full bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <div className="flex h-full items-center justify-center">
-        <div className="w-[95%] rounded-full bg-green-700 px-5 py-2">
+        <div className="h-3/5 w-[95%] rounded-full bg-green-700 px-5 py-2 lg:w-3/5 xl:w-1/2">
           <Link
             href="/checkout"
             className="flex items-center justify-between text-sm font-medium text-white"
