4834849b173e5194670965a38b35aa6c4e07decc
devden7 <devdenn7@gmail.com>
2024-08-29 07:56:25 +0700

    fix: error deployment

--- provider/TanStackProvider.tsx
+++ provider/TanStackProvider.tsx
@@ -2,17 +2,10 @@
 
 import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
 const TanStackProvider = ({ children }: { children: React.ReactNode }) => {
   const [client] = useState(() => new QueryClient());
-  return (
-    <QueryClientProvider client={client}>
-      {/* <ReactQueryStreamedHydration> */}
-      {children}
-      {/* </ReactQueryStreamedHydration> */}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
 
 export default TanStackProvider;
